a60ba4f828120aec187735ed2a4a6f378ff95513
huda19XYZ <mohfhuda19@gmail.com>
2017-03-16 12:54:28 +0700

    Create sticky.js

---
+++ sticky.js
@@ -0,0 +1,82 @@
+(function($) {
+$.fn.sticky= function(minWidth= 0) {
+var wrapper= $(this).css({
+"min-height": "1px",
+"min-width": "1px",
+"position": "static"
+}).wrapInner('<div>');
+wrapper.parent().css('position', 'relative');
+$(window).on('scroll resize sticky', function() {
+if(this.innerWidth<minWidth) {
+wrapper.children().removeAttr('class style');
+return;
+}
+var heights= new Array();
+wrapper.children().each(function() {
+heights.push($(this).outerHeight());
+});
+var parHeight= wrapper.parent().outerHeight(),
+winScroll= $(this).scrollTop(),
+winHeight= this.innerHeight,
+winWidth= this.innerWidth,
+grow= winHeight+winScroll-parHeight;
+wrapper.each(function(index) {
+var heightest= Math.max.apply(null, heights),
+sticky= $(this).children(),
+height= sticky.outerHeight(),
+width= $(this).outerWidth(),
+kelas= $(this).attr('class'),
+left= $(this).offset().left,
+top= $(this).offset().top,
+pull= left-$(this).parent().offset().left;
+if(index==heights.indexOf(heightest)) {
+sticky.removeAttr('class style');
+return this;
+}
+sticky.addClass($(this).attr('class'));
+if(height+top>=winHeight) {
+if(height+top<=winScroll+winHeight) {
+if(grow>0) sticky.css({
+"position": "absolute",
+"bottom": 0,
+"width": width,
+"left": pull,
+"top": "auto"
+});
+else sticky.css({
+"position": "fixed",
+"bottom": 0,
+"width": width,
+"left": left,
+"top": "auto"
+});
+}
+else sticky.css({
+"position": "absolute",
+"bottom": "auto",
+"width": width,
+"left": pull,
+"top": 0
+});
+}
+else {
+if(grow>winHeight-height) sticky.css({
+"position": "absolute",
+"bottom": 0,
+"width": width,
+"left": pull,
+"top": "auto"
+});
+else sticky.css({
+"position": "fixed",
+"bottom": "auto",
+"width": width,
+"left": left,
+"top": top,
+});
+}
+});
+}).trigger('sticky');
+return this;
+}
+})(jQuery);
